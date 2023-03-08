16e8ceb6a0cb61327e85dfe2432d98157d708c35
Donovan Porter <donovanporter@ufl.edu>
2023-03-07 21:07:18 -0500

    v. 2.0 Added encoder functionality for 7, 8, and 9. Added decoder function.

--- basic_encode.py
+++ basic_encode.py
@@ -4,11 +4,40 @@
 # The now "encoded" password is returned.
 def encode(raw_password):
     encoded_password = ""
+    # Adding functionality for numbers which would be incremented to double digits. -DP
     for i in raw_password:
         i = int(i)
-        i += 3
+        if i == 7:
+            i -= 7
+        elif i == 8:
+            i -= 7
+        elif i == 9:
+            i -= 7
+        else:
+            i += 3
+        # ... I got some very weird bugs trying the preceding as a shorter statement...  -DP
         encoded_password = encoded_password + str(i)
+        print(encoded_password)
     return encoded_password
+
+
+# Decoding function which takes an encoded hash and decrements them by three.
+# The now "decoded" password is returned.    -DP
+def decode(hashed_password):
+    decoded_password = ""
+    for i in hashed_password:
+        i = int(i)
+        if i == 0:
+            i += 7
+        elif i == 1:
+            i += 7
+        elif i == 2:
+            i += 7
+        else:
+            i -= 3
+        decoded_password = decoded_password + str(i)
+        print(decoded_password)
+    return decoded_password
 
 
 # Here is the main function, containing the main body and loop of the program.
@@ -32,8 +61,8 @@ def main():
             stored_encoded = encode(stored_original)
             print('Your password has been encoded and stored! \n')
         if input_option == 2:
-# This part I have left intentionally unfinished.
-            print('Run the decode function.')
+            stored_decoded = decode(stored_encoded)
+            print(f'The encoded password is {stored_encoded}, and the original password is {stored_decoded}')
         if input_option == 3:
             loop_var = 1
             break
